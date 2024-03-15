365a37dd1df319da814eff9650d1457832e8f6b0
Aligern <sasso.luca93@gmail.com>
2024-03-15 18:15:46 +0100

    not finished

--- Js/script.js
+++ Js/script.js
@@ -2,19 +2,22 @@
 
 // we create a variable to call our button from our index.html:
 let elBtnPlay = document.getElementById("startBtn");
-console.log(elBtnPlay);
-
 
 // this button will make the game start:
 elBtnPlay.addEventListener('click', function() {
     console.log('iniziamo a giocare!');
-
+    createGrid();
 }
 );
 
+function executeOnClick(cell) {
+    cell.classList.add('ls-blue');
+    console.log(cell.textContent);
+};
 
 //we do a function to create our grid into our "mineField" container:
 function createGrid () {
+    // this constant calls our "mineField"
     const mineField = document.getElementById("mineField");
     // here we define the number of cell we need:
     const cellCount = 100;
@@ -22,13 +25,12 @@ function createGrid () {
     for (let i = 0; i < cellCount; i++) {
         const cell = document.createElement("div");
         // here we add to our cells the classes we want to give them:
-        cell.classList.add("ls-cell");
+        cell.classList.add("ls-cell", "d-flex");
         // with cell.textContent we are telling the machine to print sequentially the text
-        cell.textContent = i - 1;
+        cell.textContent = i + 1;
+        cell.addEventListener('click', function() {
+            executeOnClick(cell);
+        });
         mineField.appendChild(cell);   
     }
-}
-
-//when the user clicks on play a grid of cells has to happear into our "mineField"
-
-//the user will be able to click on each of the squares inside the minefield and a color and a number will happear into these boxes
+};
